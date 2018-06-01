7b472813f984c24fd221b1b4012ec992042280cb
Bob Schneider <schmedly99@gmail.com>
2018-06-01 15:01:37 -0700

    add buttons

--- app/src/components/pages/schools/schools.jsx
+++ app/src/components/pages/schools/schools.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import axios from "axios";
+  
+  class Schools extends React.Component {
+    state = {
+      users: []
+    };
+  
+    componentWillMount() {
+      axios.get("https://randomuser.me/api/").then(user => {
+        this.setState({
+          users: user.data.results
+        });
+        // console.log(user.data.results);
+      });
+    }
+  
+    render() {
+      console.log(this.state.users);
+      return <p>Testing...</p>;
+    }
+  };
+  
 
-function schools() {
-  return <p>bob goes to school, bob wants to code</p>;
-}
-export default schools;
+
+
+  export default Schools;
+
+
+
+
+

37b4eef238ef4727164d11d54327eb8f8fba16c8
Bob Schneider <schmedly99@gmail.com>
2018-06-04 15:54:18 -0700

    commiting...

--- app/src/components/pages/Home/index.jsx
+++ app/src/components/pages/Home/index.jsx
@@ -53,6 +53,46 @@ function Program() {
     <div>
       <p>{tempData.major}</p>
       <p>{tempData.degree}</p>
+      <p>{tempData.degreetype}</p>
+      <p>{tempData.schoolname}</p>
+      <p>{tempData.tuition}</p>
+      <p>{tempData.summary}</p>
+      <p>{tempData.website}</p>
+      <p>{tempData.map}</p>
+      <p>{tempData.photo}</p>
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src="tempData.photo" alt="school image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div classNameName="media-left">
+            <figure class="image is-48x48">
+              <img src="tempData.map" alt="map image" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-4">tempData.schoolname</p>
+            <p className="subtitle is-6" />
+          </div>
+        </div>
+
+        <div className="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br />
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
     </div>
   );
 }
